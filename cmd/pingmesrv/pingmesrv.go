aa6199839b7e6cb45b56f92d96003cff6e9ab515
Antoine Pourchet <antoine.pourchet@gmail.com>
2016-07-12 19:13:23 +0200

    Systemd support added

--- cmd/pingmesrv/pingmesrv.go
+++ cmd/pingmesrv/pingmesrv.go
@@ -2,21 +2,48 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/apourchet/pingme/lib"
+	"io/ioutil"
 	"log"
 )
 
+const (
+	systemdFilename = "/etc/systemd/system/pingme.service"
+	systemdFile     = `[Unit]
+Description=Pingme
+
+[Service]
+TimeoutStartSec=0
+ExecStart=pingmesrv -p %s
+
+[Install]
+WantedBy=multi-user.target
+`
+)
+
 var (
-	port *string
+	systemd *bool
+	port    *string
 )
 
 func init() {
+	systemd = flag.Bool("systemd", false, "pingmesrv -systemd\nInstalls systemd unit file")
 	port = flag.String("p", ping.DEFAULT_PORT, "Port to listen on")
 	flag.Parse()
 }
 
 func main() {
-	s := ping.NewServer(*port)
-	log.Println("Listening on port " + *port)
-	s.Serve()
+	if *systemd {
+		file := fmt.Sprintf(systemdFile, *port)
+		err := ioutil.WriteFile(systemdFilename, []byte(file), 0644)
+		if err != nil {
+			log.Println("Error: Could not write to", systemdFilename)
+			log.Println(err)
+		}
+	} else {
+		s := ping.NewServer(*port)
+		log.Println("Listening on port " + *port)
+		s.Serve()
+	}
 }
